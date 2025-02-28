d8321fb82642b5ca0154abbac1be8097b243c79a
cehinds <cehinds@alaska.edu>
2025-02-28 09:40:56 -0900

    fixed a bug where None was returned instead of prop_results; reverted to first query style instead;

--- SQLiteProject/vulnerable_sql.py
+++ SQLiteProject/vulnerable_sql.py
@@ -2,27 +2,45 @@ import sqlite3
 
 #Probes DB for vulnerablilities and attempts to extract all users
 def get_user_credentials(username, db = 'test.db'):
-    print(f"SQLite3: Attempting to connect to {db} ...") #Debug  
-    db_connection = sqlite3.connect(db) #attempt to connect to db
-    print(f"SQLite3: Established connection to {db}! ") #Debug
+    try: 
+        print(f"SQLite3: Attempting to connect to {db} ...") #Debug  
+        db_connection = sqlite3.connect(db) #attempt to connect to db
+        print(f"SQLite3: Established connection to {db}! ") #Debug
+        
 
-    print(f"SQLite3: Initializing cursor ...") #Debug
-    db_cursor = db_connection.cursor() #creates cursor
-    print(f"SQLite3: Cursor initialized!") #Debug
+        print(f"SQLite3: Initializing cursor ...") #Debug
+        db_cursor = db_connection.cursor() #creates cursor
+        print(f"SQLite3: Cursor initialized!") #Debug
 
-    #check for vulnerable query in db
-    probe_query = f"select * from users where username ='{username}'"
-    print(f"Executing Query: {probe_query}") #DEBUG
+        #check for vulnerable query in db
+        probe_query1 = f"select * from users where username ='{username}'"
+        #probe_query2 = f"SELECT * FROM users WHERE username = ?"
 
-    db_cursor.execute(probe_query)
+        print(f"Executing Query: {probe_query1}") #DEBUG
 
-    #if successful, collects all user data
-    probe_result = db_cursor.fetchall() #returns list of tuples or an empty list
+        db_cursor.execute(probe_query1)
 
-    #close connection
-    db_connection.close()
+        #if successful, collects all user data
+        probe_result = db_cursor.fetchall() #returns list of tuples or an empty list
+
+        #close connection
+        db_connection.close()
+        return probe_result
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+    
+    finally:
+        #close connection
+        if db_connection:
+            db_connection.close()
 
 
 #test SQL injection
 inj_prompt1 = "'OR '1' = '1" #statement is true, should return all users!
+print(f"Testing inj_prompt 1 with malicious imput: {inj_prompt1}")
 print(get_user_credentials(inj_prompt1))
+
+#test normal input
+normal_input = "alice"
+print(f"Testing inj_prompt1 with normal input: {normal_input}")
+print (get_user_credentials(normal_input))
