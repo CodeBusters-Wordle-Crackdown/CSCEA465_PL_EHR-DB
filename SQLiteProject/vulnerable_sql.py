80bd32f0c1a69c25bc37eb5ceed35a98b6cea0e3
cehinds <cehinds@alaska.edu>
2025-02-28 09:10:44 -0900

    created get_user_credetials_probe function that tries to get all user names and print them

---
+++ SQLiteProject/vulnerable_sql.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def get_user_credentials(username):
+    db_connection = sqlite3.connect('test.db')
+    db_cursor = db_connection.cursor()
+
+    #check for vulnerable query in db
+    probe_query = f"select * from users where username ='{username}'"
+    db_cursor.execute(probe_query)
+
+    #if successful, collects all user data
+    probe_result = db_cursor.fetchall()
+
+    #close connection
+    db_connection.close()
+
+
+#test SQL injection
+inj_prompt1 = " 'OR '1' = '1' " #statement is true, should return all users!
+print(get_user_credentials(inj_prompt1))
