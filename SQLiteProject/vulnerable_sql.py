2e5f43d96a0f20114c5f5c2dfb69cf33d6183e21
cehinds <cehinds@alaska.edu>
2025-02-28 09:38:59 -0900

    wrapped get_user_creditials in try block; added alternate quary style and prompt, checked if db connection was active before closing; added a normal input ("alice") for user name after malicious input

--- SQLiteProject/vulnerable_sql.py
+++ SQLiteProject/vulnerable_sql.py
@@ -1,16 +1,23 @@
 import sqlite3
 
 #Probes DB for vulnerablilities and attempts to extract all users
-def get_user_credentials(username):
-    db_connection = sqlite3.connect('test.db')
-    db_cursor = db_connection.cursor()
+def get_user_credentials(username, db = 'test.db'):
+    print(f"SQLite3: Attempting to connect to {db} ...") #Debug  
+    db_connection = sqlite3.connect(db) #attempt to connect to db
+    print(f"SQLite3: Established connection to {db}! ") #Debug
+
+    print(f"SQLite3: Initializing cursor ...") #Debug
+    db_cursor = db_connection.cursor() #creates cursor
+    print(f"SQLite3: Cursor initialized!") #Debug
 
     #check for vulnerable query in db
     probe_query = f"select * from users where username ='{username}'"
+    print(f"Executing Query: {probe_query}") #DEBUG
+
     db_cursor.execute(probe_query)
 
     #if successful, collects all user data
-    probe_result = db_cursor.fetchall()
+    probe_result = db_cursor.fetchall() #returns list of tuples or an empty list
 
     #close connection
     db_connection.close()
